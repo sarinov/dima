3431fdced7e4faff95fc721fe0e746d3edaca1c9
Decode <dima.perm11@gmail.com>
2022-05-31 10:29:43 +0600

    ip

--- public/js/socket.js
+++ public/js/socket.js
@@ -1,4 +1,31 @@
 $(document).ready(function() {
+// const os = require('os');
+//
+// var ifaces = os.networkInterfaces();
+//
+// let local;
+//
+//
+// Object.keys(ifaces).forEach(function (ifname) {
+//   var alias = 0;
+//
+//   ifaces[ifname].forEach(function (iface) {
+//     if ('IPv4' !== iface.family || iface.internal !== false) {
+//       return;
+//     }
+//
+//     if (alias >= 1) {
+//       console.log(ifname + ':' + alias, iface.address);
+//     } else {
+//         local = iface.address.split(".");
+//
+//         if (local[0] == 192) {
+//             local = local.join(".")
+//         }
+//     }
+//     ++alias;
+//   });
+// });
 
 let user = localStorage.getItem('user_data');
 
@@ -6,9 +33,9 @@ if(!user) return;
 
 user = JSON.parse(user)
 
-var socket = io("ws://192.168.0.155:5001", { query: { id: user.id }});
+var socket = io(`ws://192.168.1.66:5001`, { query: { id: user.id }});
 
-  
+
 socket.on('chat message', function(msg) {
     // var item = document.createElement('li');
     // item.textContent = msg;
