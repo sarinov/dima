4514f85c1901ed61ce87a9b65d6a843edd952165
_sarinov <eskendirsarinov@gmail.com>
2022-06-03 14:23:15 +0600

    ашчч

--- public/js/socket.js
+++ public/js/socket.js
@@ -2,8 +2,8 @@
 $(document).ready(function() {
 
 let user = localStorage.getItem('user_data');
-let audio = document.getElementById('audio');
-audio.setAttribute("allow", "accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture");
+
+
 // function playSound () {
 //     let ding = new Audio('/audio/sms.mp3');
 //     ding.play();
@@ -14,11 +14,28 @@ user = JSON.parse(user)
 
 var socket = io("ws://192.168.0.156:5001", { query: { id: user.id }});
 
-
+var audio = new Audio();
+audio.src='/audio/sms.mp3';
+// when the sound has been loaded, execute your code
+audio.oncanplaythrough = (event) => {
+    var playedPromise = audio.play();
+    if (playedPromise) {
+        playedPromise.catch((e) => {
+             console.log(e)
+             if (e.name === 'NotAllowedError' || e.name === 'NotSupportedError') { 
+                   console.log(e.name);
+              }
+         }).then(() => {
+              console.log("playing sound !!!");
+         });
+     }
+}
 
 socket.on('sendMessage', function(msg) {
 
     if(user.id == msg.toId){
+        // let audio = document.getElementById('audio_sms');
+        // console.log(audio);
         audio.play()
         var templateResponse = Handlebars.compile( $("#message-response-template").html());
         console.log(msg.content)
